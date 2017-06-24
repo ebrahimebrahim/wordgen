828f191f208ed022a7618e9eb4ec1c8a162a246b
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2017-06-24 06:53:46 -0700

    unconstrained syllable generation works

--- wordgen.py
+++ wordgen.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python2.7
 
+import csv,random
+
 # A syllable is a tuple consisting of an onset, a nucleus, and a coda,
 # each of which is a list of Sounds.
 # A word is a list of syllables.
 
-# A language is a pair consisting of a list of Sounds and a list of phonotactic constraints,
-# where a phonotactic constraint is a function that, given a syllable, returns whether or not the syllable passes.
 
 class Sound(object):
   """ A phone/phoneme (don't care about difference for now).
@@ -17,7 +17,14 @@ class Sound(object):
         row: row of table representing this sound, a list of strings
   """
   def __init__(self, header, row):
-    raise Exception("not implemented")
+    for key,val in zip(header,row):
+      if key[0:3] == "is_" :
+        val = True if val == 'y' else False
+      self.__dict__[key]=val
+    self.is_consonant = not self.is_vowel
+    self.is_continuant = not self.is_stop and not self.is_affricate
+    self.is_obstruent = self.is_fricative or (self.is_stop and not self.is_nasal)
+    self.is_nucleus = self.is_vowel or self.is_liquid or self.is_nasal
 
   def display(self,style):
     """ Returns string to display sound, in given style.
@@ -25,38 +32,70 @@ class Sound(object):
           'IPA': print in IPA
           'English': print in English
     """
-    raise Exception("not implemented")
+    return self.__dict__["display_"+style]
 
 
-# TODO: define "english"
-
-
-def gen_onset(sounds):
-  raise Exception("not implemented")
-
-def gen_nucleus(sounds):
-  raise Exception("not implemented")
-
-def gen_coda(sounds):
-  raise Exception("not implemented")
-
-def phonotactic(syllable,constraints):
-  """ Returns True if syllable meets phonotactic constraints.
-      Otherwise returns False, and returns False if syllable is None.
+class Language(object):
+  """ Should consist of a list of Sounds, which can be imported from a csv file,
+      and some lists of phonotactic constraints.
+      Each constraint list should be a list of functions that return True/False based on whether something passes a constraint.
+      For example, onset_constraints would be a list of functions each of which takes an onset and returns whether that onset passes a constraint.
   """
-  if syllable is None: return False
-  return all(constraint_holds(syllable) for constraint_holds in constraints)
+  def __init__(self):
+    self.sounds = []
+    self.onset_constraints = []
+    self.coda_constraints = []
+    self.syllable_constraints = []
+    self.word_constraints = []
+
+    # these will determine the probability of each onset/coda length showing up in generation
+    # for example onset_length_distr==[0.25,0.5] means a 25% chance of 0 or 1 consonant onset, and 50% chance of 2 consonant onset
+    self.onset_length_distr = []
+    self.coda_length_distr = []
+    
+  def import_sound_list(self,csv_filename):
+    f=open(csv_filename)
+    rows = [row for row in csv.reader(f)]
+    f.close()
+    self.sounds = [Sound(rows[0],row) for row in rows[1:]]
+
+english = Language()
+english.import_sound_list("english_sounds.csv")
+english.onset_length_distr = [0.3,0.6,0.9]
+english.coda_length_distr = [0.8/3,2*0.8/3,0.8,0.95]
+# TODO: add constraints
+
+
+def gen_onset(language):
+  onset = None
+  r = random.random()
+  onset_len = len([x for x in language.onset_length_distr if r > x])
+  while onset is None or not all(constraint_holds(onset) for constraint_holds in language.onset_constraints):
+    onset = [random.choice([sound for sound in language.sounds if sound.is_consonant]) for i in range(onset_len)]
+  return onset
+
+def gen_nucleus(language):
+  return [random.choice([sound for sound in language.sounds if sound.is_nucleus])]
+
+def gen_coda(language):
+  coda = None
+  r = random.random()
+  coda_len = len([x for x in language.coda_length_distr if r > x])
+  while coda is None or not all(constraint_holds(coda) for constraint_holds in language.coda_constraints):
+    coda = [random.choice([sound for sound in language.sounds if sound.is_consonant]) for i in range(coda_len)]
+  return coda
 
 def gen_syllable(language):
-  sounds,constraints = language
   syllable = None
-  while not phonotactic(syllable,constraints):
-    syllable = (gen_onset(sounds), gen_nucleus(sounds), gen_coda(sounds))
+  while syllable is None or not all(constraint_holds(syllable) for constraint_holds in language.syllable_constraints):
+    syllable = (gen_onset(language), gen_nucleus(language), gen_coda(language))
   return syllable
 
-
 def gen_word(num_syllables,language):
-  return [gen_syllable(language) for i in range(num_syllables)]
+  word = None
+  while word is None or not all(constraint_holds(word) for constraint_holds in language.word_constraints):
+    word = [gen_syllable(language) for i in range(num_syllables)]
+  return word
 
 
 
